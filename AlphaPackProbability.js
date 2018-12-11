b5a4368cda1bcdd36ddbe799506337ed92e8bffa
SavageVector <45766399+SavageVector@users.noreply.github.com>
2018-12-10 21:50:29 -0500

    Update AlphaPackProbability.js

--- AlphaPackProbability.js
+++ AlphaPackProbability.js
@@ -49,7 +49,7 @@ function CalculateAlphaProbability() {
 }
 
 function GetTextboxValues() {
-  GameWinChance = document.getElementById("TextWinrate").innerText;
+  GameWinChance = parseInt(document.getElementById("TextWinrate").innerText, 10);
 }
 function SetTextboxValues(ChancePerRound, AverageNumberOfGames) {
   document.getElementById("TextChancePerRound").innerText = ChancePerRound;
