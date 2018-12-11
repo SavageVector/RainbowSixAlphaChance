3bb5c3087b9b3d821f6d076a78f3ba165afa89e2
SavageVector <45766399+SavageVector@users.noreply.github.com>
2018-12-11 00:41:37 -0500

    Update AlphaPackProbability.js

--- AlphaPackProbability.js
+++ AlphaPackProbability.js
@@ -6,23 +6,20 @@ function CalculateAlphaProbability() {
   var AlphapackWinChanceArray = [];
   var GameAlphapackWinChanceSum;
   var AlphapackAverageGames = 0;
+  var i;
   GetTextboxValues();
   AlphapackWinChanceArray[0] = AlphapackWinChanceAddWin;
-  
   //Populate array with the average Alphapack chance at game "i"
-  var i;
   for (i = 1; i < GameTotalCount; i++) {
     AlphapackWinChanceArray[i] = AlphapackWinChanceArray[i-1] + (GameWinChance * AlphapackWinChanceAddWin) + ((1 - GameWinChance) * AlphapackWinChanceAddLoss);
     if(AlphapackWinChanceArray[i] > 1) {
       AlphapackWinChanceArray[i] = 1;
     }
   }
-  
   //Populate array with the average chance of winning Alphapack for game "i"
   for (i = 0; i < AlphapackWinChanceArray.length; i++) {
     AlphapackWinChanceArray[i] = AlphapackWinChanceArray[i] * GameWinChance;
   }
-  
   //Populate array with the average chance of getting to game "i", and winnning
   GameAlphapackWinChanceSum = AlphapackWinChanceArray[0];
   for (i = 1; i < AlphapackWinChanceArray.length; i++) {
@@ -32,19 +29,14 @@ function CalculateAlphaProbability() {
       GameAlphapackWinChanceSum = 1;
     }
   }
-  
-  //AlphapackWinChanceArray.length
-  
+  //Calculate average games before Alphapack win
   for (i = 0; i < AlphapackWinChanceArray.length; i++) {
     AlphapackAverageGames = AlphapackAverageGames + (AlphapackWinChanceArray[i] * (i + 1));
   }
-  
-  
-  //document.getElementById("TextChancePerRound").innerText = AlphapackAverageGames;
-  
-  
+  //Update page
   SetTextboxValues(1 / AlphapackAverageGames, AlphapackAverageGames);
-  
+  //WIP
+  DrawChart();
   SetTable();
 }
 
@@ -59,33 +51,5 @@ function SetTable() {
   //WIP
 }
 function DrawChart() {
-  var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	theme: "light2",
-	title:{
-		text: "Simple Line Chart"
-	},
-	axisY:{
-		includeZero: false
-	},
-	data: [{
-		type: "line",
-      dataPoints: [
-        { y: 450 },
-        { y: 414 },
-        { y: 520 },
-        { y: 460 },
-        { y: 450 },
-        { y: 500 },
-        { y: 480 },
-        { y: 480 },
-        { y: 410 },
-        { y: 500 },
-        { y: 480 },
-        { y: 510 }
-      ]
-    }]
-  });
-  chart.render();
-  }
+  //WIP
 }
