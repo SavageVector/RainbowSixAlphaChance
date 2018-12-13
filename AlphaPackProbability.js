306912fce598628dec806135fb0c9b9784991a4a
SavageVector <45766399+SavageVector@users.noreply.github.com>
2018-12-13 14:55:43 -0500

    Update AlphaPackProbability.js

--- AlphaPackProbability.js
+++ AlphaPackProbability.js
@@ -4,6 +4,7 @@ var GameCountMax = 10000;
 var GameWinChance = 0.5;
 function CalculateAlphaProbability() {
   var AlphapackWinChanceArray = [];
+  var AlphapackWonChanceArray = [];
   var AlphapackWinChanceAverage;
   var GameAlphapackWinChanceSum;
   var AlphapackAverageGames = 0;
@@ -11,15 +12,20 @@ function CalculateAlphaProbability() {
   GetTextboxValues();
   GameAlphapackWinChanceSum = 0;
   AlphapackWinChanceAverage = AlphapackWinChanceAddWin;
+  
+  AlphapackWinChanceArray[0] = AlphapackWinChanceAddWin * GameWinChance;
+  AlphapackWonChanceArray[0] = AlphapackWinChanceArray[0];
+  
   Count = 0;
-  while ((GameAlphapackWinChanceSum < 0.9999) && (Count < GameCountMax)) {
-    AlphapackWinChanceArray[Count] = (AlphapackWinChanceAverage * GameWinChance) * (1 - GameAlphapackWinChanceSum);
+  while ((AlphapackWonChanceArray[Count] < 0.9999) && (Count <= GameCountMax)) {
+    Count++;
+    AlphapackWinChanceArray[Count] = (AlphapackWinChanceAverage * GameWinChance) * (1 - AlphapackWonChanceArray[Count - 1]);
+    AlphapackWonChanceArray[Count] = AlphapackWonChanceArray[Count - 1] + AlphapackWinChanceArray[Count];
     GameAlphapackWinChanceSum = GameAlphapackWinChanceSum + AlphapackWinChanceArray[Count];
     AlphapackWinChanceAverage += (GameWinChance * AlphapackWinChanceAddWin) + ((1 - GameWinChance) * AlphapackWinChanceAddLoss);
     if (AlphapackWinChanceAverage > 1) {
       AlphapackWinChanceAverage = 1;
     }
-    Count++;
   }
   //Calculate average games before Alphapack win
   for (Count = 0; Count < AlphapackWinChanceArray.length; Count++) {
@@ -29,7 +35,7 @@ function CalculateAlphaProbability() {
   SetTextboxValues(1 / AlphapackAverageGames, AlphapackAverageGames);
   //WIP
   DrawChart(document.getElementById("ChartPrint"),AlphapackWinChanceArray);
-  DrawTable(document.getElementById("TablePrint"),AlphapackWinChanceArray);
+  //DrawTable(document.getElementById("TablePrint"),AlphapackWinChanceArray);
   
   //document.getElementById("TempOutput").innerText = AlphapackWinChanceArray.length;
 }
@@ -73,75 +79,6 @@ function DrawTable(TableDiv, Array) {
   TableDiv.appendChild(tbl);
 }
 
-
-
-
-
-
-
-
-
-
-function DrawChart(Canvas, DataArray) {
-  var xPadding = 30;
-  var yPadding = 30;
-  var Chart = Canvas.getContext('2d');
-  Chart.clearRect(0, 0, Canvas.width, Canvas.height);
-  //Set chart styling
-  Chart.lineWidth = 2;
-  Chart.strokeStyle = '#333';
-  Chart.font = 'italic 8pt sans-serif';
-  Chart.textAlign = "center";
-  //Draw axes
-  Chart.beginPath();
-  Chart.moveTo(xPadding, yPadding);
-  Chart.lineTo(xPadding, Canvas.height - yPadding);
-  Chart.lineTo(Canvas.width, Canvas.height - yPadding);
-  Chart.stroke();
-  //Label Y axis
-  for(var i = 0; i < GetArrayMaxValue(DataArray); i += 0.01) {
-    Chart.fillText(Math.round(i / 0.01) * 0.01, xPadding - 15, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * i)) - yPadding);
-  }
-  //Label X axis
-  for(var i = 0; i < DataArray.length; i += DataArray.length/10) {
-    //( * i) + (xPadding * 1.5)
-    Chart.fillText(Math.round(i), ((Canvas.width - xPadding) / DataArray.length) * i + xPadding, Canvas.height - yPadding + 20);
-  }
-  //Draw Line
-  Chart.strokeStyle = '#f00';
-  Chart.beginPath();
-  Chart.moveTo((xPadding), Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * DataArray[0]) - yPadding);
-  
-  for(var i = 0; i < DataArray.length; i ++) {
-    //( * i) + (xPadding * 1.5)
-      Chart.lineTo(((Canvas.width - xPadding) / DataArray.length) * i + xPadding, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * DataArray[i])) - yPadding);
-  }
-  Chart.stroke();
-  //Max value line
-  Chart.strokeStyle = '#333';
-  Chart.lineWidth = 1;
-  Chart.beginPath();
-  Chart.moveTo(xPadding, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * GetArrayMaxValue(DataArray))) - yPadding);
-  Chart.lineTo(((Canvas.width - xPadding) / DataArray.length) * GetArrayMaxPosition(DataArray) + (xPadding * 1.5), (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * GetArrayMaxValue(DataArray))) - yPadding);
-  Chart.stroke();
-  
-  
-  
-  Chart.fillText(Math.round(1000 * GetArrayMaxValue(DataArray)) / 1000, xPadding + 15, ((Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * GetArrayMaxValue(DataArray))) - yPadding) + 15);
-  Chart.stroke();
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  document.getElementById("TempOutput").innerText = DataArray[10];
-}
 
 
 
