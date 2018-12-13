57dc46d84cc82354aef5d9635ac8cbed501c1a61
SavageVector <45766399+SavageVector@users.noreply.github.com>
2018-12-13 16:33:00 -0500

    Update DrawCanvas.js

--- DrawCanvas.js
+++ DrawCanvas.js
@@ -1,49 +1,84 @@
-function DrawChart(Canvas, DataArray) {
+function DrawChart(Canvas, DataArray1, DataArray2) {
   var xPadding = 30;
   var yPadding = 30;
   var Chart = Canvas.getContext('2d');
   Chart.clearRect(0, 0, Canvas.width, Canvas.height);
-  //Set chart styling
-  Chart.lineWidth = 2;
-  Chart.strokeStyle = '#333';
-  Chart.font = 'italic 8pt sans-serif';
-  Chart.textAlign = "center";
   //Draw axes
+  Chart.lineWidth = 2;
+  Chart.strokeStyle = "black";
+  Chart.globalAlpha = 1;
   Chart.beginPath();
   Chart.moveTo(xPadding, yPadding);
   Chart.lineTo(xPadding, Canvas.height - yPadding);
-  Chart.lineTo(Canvas.width, Canvas.height - yPadding);
+  Chart.lineTo(Canvas.width - xPadding, Canvas.height - yPadding);
   Chart.stroke();
   //Label Y axis
-  for(var i = 0; i < GetArrayMaxValue(DataArray); i += 0.01) {
-    Chart.fillText(Math.round(i / 0.01) * 0.01, xPadding - 15, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * i)) - yPadding);
+  Chart.font = 'italic 8pt sans-serif';
+  Chart.textAlign = "center";
+  for(var i = 0; i < GetArrayMaxValue(DataArray1) + 0.1; i += 0.1) {
+    Chart.fillText(Math.round(i * 10) / 10, xPadding - 15, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray1) + 0.2)) * i)) - yPadding);
   }
   //Label X axis
-  for(var i = 0; i < DataArray.length; i += DataArray.length/10) {
-    //( * i) + (xPadding * 1.5)
-    Chart.fillText(Math.round(i), ((Canvas.width - xPadding) / DataArray.length) * i + xPadding, Canvas.height - yPadding + 20);
+  Chart.font = 'italic 8pt sans-serif';
+  Chart.textAlign = "center";
+  for(var i = 0; i < DataArray1.length ; i ++) {
+    if(0 == i % Math.round(DataArray1.length / 10)) {
+      Chart.fillText(i, ((Canvas.width - (xPadding * 2)) / DataArray1.length) * i + xPadding, Canvas.height - yPadding + 20);
+    }
   }
-  //Draw Line
-  Chart.strokeStyle = '#f00';
-  Chart.beginPath();
-  Chart.moveTo((xPadding), Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * DataArray[0]) - yPadding);
   
-  for(var i = 0; i < DataArray.length; i ++) {
-    //( * i) + (xPadding * 1.5)
-      Chart.lineTo(((Canvas.width - xPadding) / DataArray.length) * i + xPadding, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * DataArray[i])) - yPadding);
-  }
-  Chart.stroke();
-  //Max value line
-  Chart.strokeStyle = '#333';
+  //Draw Line 1
   Chart.lineWidth = 1;
+  Chart.strokeStyle = "blue";
+  Chart.globalAlpha = 1;
   Chart.beginPath();
-  Chart.moveTo(xPadding, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * GetArrayMaxValue(DataArray))) - yPadding);
-  Chart.lineTo(((Canvas.width - xPadding) / DataArray.length) * GetArrayMaxPosition(DataArray) + (xPadding * 1.5), (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * GetArrayMaxValue(DataArray))) - yPadding);
+  Chart.moveTo((xPadding), Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray1) + 0.2)) * DataArray1[0]) - yPadding);
+  
+  for(var i = 0; i < DataArray1.length; i ++) {
+    //( * i) + (xPadding * 1.5)
+      Chart.lineTo(((Canvas.width - (xPadding * 2)) / DataArray1.length) * i + xPadding, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray1) + 0.2)) * DataArray1[i])) - yPadding);
+  }
   Chart.stroke();
   
   
   
-  Chart.fillText(Math.round(1000 * GetArrayMaxValue(DataArray)) / 1000, xPadding + 15, ((Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray) + 0.01)) * GetArrayMaxValue(DataArray))) - yPadding) + 15);
+  
+  //Draw Line 2
+  Chart.lineWidth = 1;
+  Chart.strokeStyle = "red";
+  Chart.globalAlpha = 1;
+  Chart.beginPath();
+  Chart.moveTo((xPadding), Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray1) + 0.2)) * DataArray2[0]) - yPadding);
+  for(var i = 0; i < DataArray2.length; i ++) {
+      Chart.lineTo(((Canvas.width - (xPadding * 2)) / DataArray2.length) * i + xPadding, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray1) + 0.2)) * DataArray2[i])) - yPadding);
+  }
+  Chart.stroke();
+  
+  
+  
+  //Draw Line 2 zoom
+  Chart.lineWidth = 1;
+  Chart.strokeStyle = "red";
+  Chart.globalAlpha = 0.5;
+  Chart.beginPath();
+  Chart.moveTo((xPadding), Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray2) + 0.01)) * DataArray2[0]) - yPadding);
+  for(var i = 0; i < DataArray2.length; i ++) {
+      Chart.lineTo(((Canvas.width - (xPadding * 2)) / DataArray2.length) * i + xPadding, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray2) + 0.01)) * DataArray2[i])) - yPadding);
+  }
+  Chart.stroke();
+  
+  
+  
+  //Max value line 2 zoom
+  Chart.strokeStyle = "black";
+  Chart.lineWidth = 1;
+  Chart.globalAlpha = 0.5;
+  Chart.beginPath();
+  Chart.moveTo(xPadding, (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray2) + 0.01)) * GetArrayMaxValue(DataArray2))) - yPadding);
+  Chart.lineTo(((Canvas.width - xPadding) / DataArray2.length) * GetArrayMaxPosition(DataArray2) + (xPadding * 1.5), (Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray2) + 0.01)) * GetArrayMaxValue(DataArray2))) - yPadding);
+  Chart.stroke();
+  
+  Chart.fillText(Math.round(1000 * GetArrayMaxValue(DataArray2)) / 1000, xPadding + 15, ((Canvas.height - (((Canvas.height - yPadding) / (GetArrayMaxValue(DataArray2) + 0.01)) * GetArrayMaxValue(DataArray2))) - yPadding) + 15);
   Chart.stroke();
   
 }
