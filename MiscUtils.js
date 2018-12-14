083fdb59be4923719bd6ea1b5e1b92c916da23dd
SavageVector <45766399+SavageVector@users.noreply.github.com>
2018-12-14 17:16:53 -0500

    Update MiscUtils.js

--- MiscUtils.js
+++ MiscUtils.js
@@ -1,31 +1,31 @@
+//Gets user input
 function GetTextboxValues() {
 	GameWinChance = parseFloat(document.getElementById("TextWinrate").value) / (parseFloat(document.getElementById("TextWinrate").value) + 1);
 }
+//Outputs two values
 function SetTextboxValues(ChancePerRound, AverageNumberOfGames) {
 	document.getElementById("TextChancePerRound").innerText = ChancePerRound;
 	document.getElementById("TextAverageNumberOfGames").innerText = AverageNumberOfGames;
 }
+//Gets largest value in an array
 function GetArrayMaxValue(DataArray) {
-  var Max = 0;
-  for(var i = 0; i < DataArray.length; i ++) {
-      if(DataArray[i] > Max) {
-        Max = DataArray[i];
-      }
-  }
-  return Max;
+	var Max = 0;
+	for(var i = 0; i < DataArray.length; i ++) {
+		if(DataArray[i] > Max) {
+			Max = DataArray[i];
+		}
+	}
+	return Max;
 }
-
+//Gets position of largest value in an array
 function GetArrayMaxPosition(Array) {
-  var Max = 0;
-  var MaxPos = 0;
-  for(var i = 0; i < Array.length; i ++) {
-      if(Array[i] > Max) {
-        Max = Array[i];
-        MaxPos = i;
-      }
-  }
-  return MaxPos;
-}
-function RoundDigits(RoundNum, NumDigits) {
-  return (Math.round(RoundNum * Math.pow(10, NumDigits)) / Math.pow(10, NumDigits))
+	var Max = 0;
+	var MaxPos = 0;
+	for(var i = 0; i < Array.length; i ++) {
+		if(Array[i] > Max) {
+			Max = Array[i];
+			MaxPos = i;
+		}
+	}
+	return MaxPos;
 }
